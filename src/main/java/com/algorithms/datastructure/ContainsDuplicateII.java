48665efa804533ddc1617cbcc43d9dd43bafa3e6
Natália L. Moreno <63927404+natalialopesmoreno@users.noreply.github.com>
2024-04-10 22:51:34 -0300

    feat: solving problem Contains Duplicate II

---
+++ src/main/java/com/algorithms/datastructure/ContainsDuplicateII.java
@@ -0,0 +1,21 @@
+package com.algorithms.datastructure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<Integer,Integer>();
+
+        for(int x = 0; x< nums.length; x++){
+            if(map.containsKey(nums[x])){
+                if(Math.abs(map.get(nums[x]) - x) <= k){
+                    return true;
+                }
+            }
+            map.put(nums[x], x);
+        }
+        return false;
+    }
+}
