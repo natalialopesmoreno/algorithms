7ca49ece368f89511a93a03d0a386394a56c25e3
Natália L. Moreno <63927404+natalialopesmoreno@users.noreply.github.com>
2024-04-10 22:18:42 -0300

    feat: solving problem Happy Number

---
+++ src/main/java/com/algorithms/datastructure/HappyNumber.java
@@ -0,0 +1,32 @@
+package com.algorithms.datastructure;
+
+import java.util.HashSet;
+
+public class HappyNumber {
+
+    public boolean isHappy(int n) {
+        HashSet<Integer> verified = new HashSet<Integer>();
+        return isNumberHappy(n,verified);
+
+    }
+
+    public boolean isNumberHappy(int n, HashSet<Integer> set){
+        if(n ==1){
+            return true;
+        }
+        int result = 0;
+        while(n>0){
+            int rem = n%10;
+            result += rem*rem;
+            n/=10;
+        }
+
+        if(!set.add(result)){
+            return false;
+        }
+
+        return isNumberHappy(result,set);
+
+
+    }
+}
