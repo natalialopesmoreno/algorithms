7bd16346c2a1724d43ac2f7908452d2b5b86aabb
Natália L. Moreno <63927404+natalialopesmoreno@users.noreply.github.com>
2024-04-16 21:41:00 -0300

    feat: solving problem Gas Station

---
+++ src/main/java/com/algorithms/datastructure/GasStation.java
@@ -0,0 +1,21 @@
+package com.algorithms.datastructure;
+
+public class GasStation {
+
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        int total_amount = 0;
+        int amount = 0;
+        int start = 0;
+
+        for(int i = 0; i < n; i++){
+            total_amount += gas[i] - cost[i];
+            amount += gas[i] - cost[i];
+            if(amount < 0){
+                amount = 0;
+                start = i + 1;
+            }
+        }
+        return (total_amount < 0) ? -1 : start;
+    }
+}
